8d1b967cbcda0ae407f5c7039017775f8d231bcc
misiteH1 <73332696+misiteH1@users.noreply.github.com>
2020-11-14 17:14:11 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
-东东萌宠 更新地址： https://raw.githubusercontent.com/lxk0301/scripts/master/jd_pet.js
-=======
 东东萌宠 更新地址： https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pet.js
->>>>>>> origin/main
 更新时间：2020-11-07
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
@@ -14,19 +10,6 @@
 =================================Quantumultx=========================
 [task_local]
 #东东萌宠
-<<<<<<< HEAD
-15 6-18/6 * * * https://raw.githubusercontent.com/lxk0301/scripts/master/jd_pet.js, tag=东东萌宠, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdmc.png, enabled=true
-
-=================================Loon===================================
-[Script]
-cron "15 6-18/6 * * *" script-path=https://raw.githubusercontent.com/lxk0301/scripts/master/jd_pet.js,tag=东东萌宠
-
-===================================Surge================================
-东东萌宠 = type=cron,cronexp="15 6-18/6 * * *",wake-system=1,timeout=120,script-path=https://raw.githubusercontent.com/lxk0301/scripts/master/jd_pet.js
-
-====================================小火箭=============================
-东东萌宠 = type=cron,script-path=https://raw.githubusercontent.com/lxk0301/scripts/master/jd_pet.js, cronexpr="15 6-18/6 * * *", timeout=200, enable=true
-=======
 15 6-18/6 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pet.js, tag=东东萌宠, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdmc.png, enabled=true
 
 =================================Loon===================================
@@ -38,7 +21,6 @@ cron "15 6-18/6 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_
 
 ====================================小火箭=============================
 东东萌宠 = type=cron,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pet.js, cronexpr="15 6-18/6 * * *", timeout=200, enable=true
->>>>>>> origin/main
 
 */
 const $ = new Env('东东萌宠');
@@ -74,17 +56,12 @@ let randomCount = 20;
       console.log(`\n开始【京东账号${$.index}】${$.nickName || $.UserName}\n`);
       if (!$.isLogin) {
         $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/`, {"open-url": "https://bean.m.jd.com/"});
-<<<<<<< HEAD
-        $.setdata('', `CookieJD${i ? i + 1 : "" }`);//cookie失效，故清空cookie。
-        if ($.isNode()) await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-=======
 
         if ($.isNode()) {
           await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
         } else {
           $.setdata('', `CookieJD${i ? i + 1 : "" }`);//cookie失效，故清空cookie。$.setdata('', `CookieJD${i ? i + 1 : "" }`);//cookie失效，故清空cookie。
         }
->>>>>>> origin/main
         continue
       }
       message = '';
